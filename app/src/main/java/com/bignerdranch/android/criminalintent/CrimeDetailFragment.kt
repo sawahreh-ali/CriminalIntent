18e797a96991519ae14fe5c67ee70ef0c06797ff
Ali <Sawahreh-ali@hotmail.com>
2022-07-08 19:50:58 +0300

    Add the first fragment

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,46 @@
+package com.bignerdranch.android.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.*
+
+class CrimeDetailFragment: Fragment() {
+    private lateinit var crime: Crime
+    private lateinit var binding: FragmentCrimeDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime(id= UUID.randomUUID(), title = "", date = Date( ), isSolved = false)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            crimeTitle.doOnTextChanged { text,_,_,_ ->
+                crime = crime.copy(title = text.toString())
+            }
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener {_, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
+    }
+}
--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -2,10 +2,12 @@ package com.bignerdranch.android.criminalintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.criminalintent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
     }
 }
